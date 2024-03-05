5e4d1bd85c44eab6d436eee1083ecdf8b7fc821d
jeffmakuto <hjeffmakuto@gmail.com>
2024-03-05 05:54:46 -0800

    Pascal's triangle algorithm

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows to generate in Pascal's Triangle.
+
+    Returns:
+        List[List[int]]: A list of lists representing Pascal's Triangle.
+
+    Examples:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+        >>> pascal_triangle(0)
+        []
+
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
